a064df86776163dca2e3fe45c2808b6c7bee19bc
Zikang CHEN <65004114+Appointat@users.noreply.github.com>
2024-07-05 09:55:34 +0200

    refactor: Update set_up.py to support multiple models in the FastAPI endpoint

--- set_up.py
+++ set_up.py
@@ -2,12 +2,14 @@ from fastapi import FastAPI, Request
 from fastapi.responses import StreamingResponse
 
 # from apps.streamlit_ui.multi_agents_async import async_main
-from apps.streamlit_ui.role_playing_async import async_main
+from apps.streamlit_ui.role_playing_evaluate_async import async_main as role_playing_evaluate_async_main
+from apps.streamlit_ui.role_playing_async_lesson_plan import async_main as role_playing_async_lesson_plan_main
+from apps.streamlit_ui.generate_quesion_and_answers import async_main as generate_quesion_and_answers_main
 
 app = FastAPI()
 
 
-async def process_request(content: str, user: dict):
+async def process_request(model_id: str, content: str, user: dict):
     # task_prompt = content
     # context_text = content
     # num_roles = 4
@@ -23,19 +25,27 @@ async def process_request(content: str, user: dict):
     #     output_language=response_language,
     # ):
     #     yield message
-    async for message in async_main(content):
-        yield message
+    if model_id == "leagent-lesson-planning":
+        async for message in role_playing_async_lesson_plan_main(content):
+            yield message
+    elif model_id == "leagent-qa":
+        async for message in generate_quesion_and_answers_main(content):
+            yield message
+    elif model_id == "leagent-evaluation":
+        async for message in role_playing_evaluate_async_main(content):
+            yield message
     yield "TASK_DONE"
 
 
 @app.post("/process")
 async def process(request: Request):
     data = await request.json()
+    model_id = data["model"]
     content = data["content"]
     user = data["user"]
 
     async def event_generator():
-        async for message in process_request(content, user):
+        async for message in process_request(model_id, content, user):
             yield f"{message}\n"
 
     return StreamingResponse(event_generator(), media_type="text/plain")
